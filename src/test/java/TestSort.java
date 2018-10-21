a7c1eb3f8b80341b04f23d63303920558c2c72ed
bigO-talent <andy1994831214asdf@gmail.com>
2018-10-21 14:31:10 +0800

    Initial commit

--- src/test/java/TestSort.java
+++ src/test/java/TestSort.java
@@ -15,7 +15,7 @@ public class TestSort {
 	}
 
 	@Test
-	public void testBubble() {
+	public void testshell() {
 		System.out.println("shellsort");
 		Sort sort = new Sort();
 		assertArrayEquals(new int[] {1,2,3}, sort.shellsort(new int[] {3,1,2}));
@@ -26,6 +26,32 @@ public class TestSort {
 		assertArrayEquals(new int[] {1,2,3,4,5,6,7,8}, sort.shellsort(new int[] {3,7,1,8,6,4,2,5}));
 		assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9}, sort.shellsort(new int[] {3,7,9,1,8,6,4,2,5}));
 		assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9,10}, sort.shellsort(new int[] {3,7,9,1,8,6,4,2,10,5}));
+	}
+	@Test
+	public void testBubble() {
+		System.out.println("bubble");
+		Sort sort = new Sort();
+		assertArrayEquals(new int[] {1,2,3}, sort.bubble(new int[] {3,1,2}));
+		assertArrayEquals(new int[] {1,2,3,4}, sort.bubble(new int[] {3,1,4,2}));
+		assertArrayEquals(new int[] {1,2,3,4,5}, sort.bubble(new int[] {5,3,1,4,2}));
+		assertArrayEquals(new int[] {1,2,3,4,5,6}, sort.bubble(new int[] {6,3,1,5,4,2}));
+		assertArrayEquals(new int[] {1,2,3,4,5,6,7}, sort.bubble(new int[] {3,7,1,6,4,2,5}));
+		assertArrayEquals(new int[] {1,2,3,4,5,6,7,8}, sort.bubble(new int[] {3,7,1,8,6,4,2,5}));
+		assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9}, sort.bubble(new int[] {3,7,9,1,8,6,4,2,5}));
+		assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9,10}, sort.bubble(new int[] {3,7,9,1,8,6,4,2,10,5}));
+	}
+	@Test
+	public void testSelection() {
+		System.out.println("selection");
+		Sort sort = new Sort();
+		assertArrayEquals(new int[] {1,2,3}, sort.selection(new int[] {3,1,2}));
+		assertArrayEquals(new int[] {1,2,3,4}, sort.selection(new int[] {3,1,4,2}));
+		assertArrayEquals(new int[] {1,2,3,4,5}, sort.selection(new int[] {5,3,1,4,2}));
+		assertArrayEquals(new int[] {1,2,3,4,5,6}, sort.selection(new int[] {6,3,1,5,4,2}));
+		assertArrayEquals(new int[] {1,2,3,4,5,6,7}, sort.selection(new int[] {3,7,1,6,4,2,5}));
+		assertArrayEquals(new int[] {1,2,3,4,5,6,7,8}, sort.selection(new int[] {3,7,1,8,6,4,2,5}));
+		assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9}, sort.selection(new int[] {3,7,9,1,8,6,4,2,5}));
+		assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9,10}, sort.selection(new int[] {3,7,9,1,8,6,4,2,10,5}));
 	}
 
 }
